db56fb35a427d584f2159177fe36b285fd5c9d0e
viip3r01001 <maximemaucourant063@gmail.com>
2025-02-19 23:18:32 +0100

    Update RamdomCraft.java

--- RamdomCraft.java
+++ RamdomCraft.java
@@ -1,11 +1,16 @@
-{\rtf1\ansi\ansicpg1252\cocoartf2709
-\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fmodern\fcharset0 Courier;}
-{\colortbl;\red255\green255\blue255;\red0\green0\blue0;}
-{\*\expandedcolortbl;;\cssrgb\c0\c0\c0;}
-\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
-\deftab720
+package com.example.randomcraft;
 
-\pard\pardeftab720\partightenfactor0
-\f0\fs26 \cf0 \expnd0\expndtw0\kerning0
-src/main/java/com/example/randomcraft/RandomCraft.java\
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class RandomCraft extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        getLogger().info("RandomCraft Plugin activé !");
+    }
+
+    @Override
+    public void onDisable() {
+        getLogger().info("RandomCraft Plugin désactivé !");
+    }
 }
